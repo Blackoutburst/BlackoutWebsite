f3bf144bdf4bb8eb35e437983144bfd0907cdedf
Blackoutburst <blackoutburst@hotmail.com>
2021-09-24 17:36:40 +0200

    ?

--- rollup.config.js
+++ rollup.config.js
@@ -29,12 +29,12 @@ function serve() {
 }
 
 export default {
-	input: 'src/wally.js',
+	input: 'src/main.js',
 	output: {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/build/wally.js'
+		file: 'public/build/bundle.js'
 	},
 	plugins: [
 		svelte({
@@ -45,7 +45,7 @@ export default {
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
-		css({ output: 'wally.css' }),
+		css({ output: 'bundle.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
