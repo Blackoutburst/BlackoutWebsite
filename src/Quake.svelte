82c42e2a9bbe25af2638c0ade8a4ff54569e8868
Blackoutburst <blackoutburst@hotmail.com>
2021-09-30 15:48:01 +0200

    Added quake page

--- src/Quake.svelte
+++ src/Quake.svelte
@@ -1,72 +1,67 @@
 <header>
     <div class="header">
-        <img src="img/pfp.jpg" alt="myLogo" class="logo">
-        <p class="title">BLACKOUTBURST</p>
+        <img src="img/quake.png" alt="myLogo" class="logo">
+        <p class="title">QUAKECRAFT</p>
     </div>
 </header>
 
-<div class="bio">
-    <h1 class="bio-title">Welcome !</h1>
+<div class="about">
+    <h1 class="about-title">About this project</h1>
     <p>
-        Hi, I'm Blackoutburst and I'm mainly a Java programmer.<br>
-        I started programming a while ago doing small Minecraft mods.<br>
+        This project doesn't really have any story<br>
+        I enjoyed playing Quake and I was wondering how hard it could be to make one<br>
+        Turns out it was really easy to do so this project was made in less than a week<br>
         <br>
-        Today I'm still working mainly on Minecraft projects that a enjoy.<br>
-        I used to do games/software with LWJGL and Slick2D.<br>
-        <br>
-        I'm a student currently doing my 3rd year inside a computer science university.<br>
-        Currently working on spigot/bukkit plugins with the goal of working at Hypixel.<br>
-        <br>
-        I go by He/Him, people usually call me Blackout so feel free to do as well!<br>
-        You can find some of my projects below!
-
+        At first it wasn't supposed to have gun customisation but some friends asked for it<br>
+        So yeah, just a simple plugin made for my own learning experience
     </p>
 </div>
 
-<div class="pr">
+<div class="features">
+    <h1 class="features-title">Features</h1>
+    <p>
+        All features are explained on the <a href="https://github.com/Blackoutburst/QuakeCraft" target="_blank" rel="noopener noreferrer">GitHub</a><br>
+        But the main ones are obviously the Railgun and the dash mechanics
+    </p>
+    <img src="img/railgun.png" alt="discord" class="features-img">
+    <p class="note">(Railgun in action)</p>
 
-    <a href="wally.html">
-        <img src="img/wally.jpg" alt="wally" class="project-img">
-    </a>
+    <p>
+        Full game only solo mode, first one to 25 point win the game
+    </p>
+    <img src="img/quake-game.png" alt="discord" class="features-img">
+    <p class="note">(Yeah i know it's literally Hypixel QuakeCraft at this point sorry)</p>
 
-    <a href="hitw.html">
-        <img src="img/hitw.jpg" alt="hitw" class="project-img">
-    </a>
-
-    <a href="quake.html">
-        <img src="img/quake.jpg" alt="quake" class="project-img">
-    </a>
-
-    <a href="https://github.com/Blackoutburst/AoW-plugin" target="_blank" rel="noopener noreferrer">
-        <img src="img/aow.jpg" alt="aow" class="project-img">
-    </a>
-
-    <a href="https://github.com/Blackoutburst/Rhythm-Pong" target="_blank" rel="noopener noreferrer">
-        <img src="img/pong.jpg" alt="pong" class="project-img">
-    </a>
-
-    <a href="https://play.google.com/store/apps/details?id=com.blackoutburst.pixelstarship" target="_blank" rel="noopener noreferrer">
-        <img src="img/ps.jpg" alt="ps" class="project-img">
-    </a>
-
-    <p class="note">(Most of my projects are on Github, only a few are listed here so recommend you to check it below)</p>
+    <p>
+        The gun customisation menu, this screenshot is only the main page<br>
+        It is divided in 3 parts the gun item, the explosion shape and explosion color
+    </p>
+    <img src="img/quake-menu.png" alt="discord" class="features-img">
+    <p class="note">(Like i said this wasn't supposed to be a feature but whatever)</p>
 
 </div>
 
-<div class="contact">
-    <h1 class="contact-title">Useful stuff</h1>
+<div class="links">
+    <h1 class="links-title">Find this project</h1>
 
-
-    <a href="javacript:void(0)" on:click={clipboard}>
-        <img src="img/discord.png" alt="discord" class="contact-img">
+    <a href="javacript:void(0)" target="_blank" rel="noopener noreferrer">
+        <img src="img/youtube.png" alt="discord" class="links-img">
     </a>
 
-    <a href="https://github.com/Blackoutburst" target="_blank" rel="noopener noreferrer">
-        <img src="img/github.png" alt="github" class="contact-img">
+    <a href="https://github.com/Blackoutburst/QuakeCraft" target="_blank" rel="noopener noreferrer">
+        <img src="img/github.png" alt="github" class="links-img">
+    </a>
+</div>
+
+<div class="links">
+    <h1 class="links-title">Made with</h1>
+
+    <a href="https://www.java.com/" target="_blank" rel="noopener noreferrer">
+        <img src="img/java.png" alt="java" class="links-img">
     </a>
 
-    <a href="https://www.youtube.com/channel/UCOmdNokN6UVeNhDfB61B_0A" target="_blank" rel="noopener noreferrer">
-        <img src="img/youtube.png" alt="youtube" class="contact-img">
+    <a href="https://www.spigotmc.org/" target="_blank" rel="noopener noreferrer">
+        <img src="img/spigot.png" alt="jda" class="links-img">
     </a>
 </div>
 
@@ -76,13 +71,6 @@
     <p class="footer-title">© Blackoutburst</p>
 </div>
 
-<script>
-function clipboard() {
-    navigator.clipboard.writeText("Blackout#0900");
-    alert("Discord copied inside the clipboard: Blackout#0900");
-}
-</script>
-
 <style>
 @font-face {
   font-family: customFontCoda;
@@ -90,7 +78,7 @@ function clipboard() {
 }
 
 @media only screen and (max-width: 1200px) {
-    .contact-img {
+    .links-img {
         max-width: 15vw;
         padding: 10px;
         margin: 10px;
@@ -98,14 +86,14 @@ function clipboard() {
 }
 
 @media only screen and (min-width: 1200px) {
-    .contact-img {
+    .links-img {
         max-width: 7%;
         padding: 10px;
         margin: 10px;
     }
 }
 
-.contact {
+.links {
     margin-top: 80px;
     padding: 10px;
     text-align: center;
@@ -113,7 +101,7 @@ function clipboard() {
 }
 
 @media only screen and (min-width: 1200px) {
-    .contact-title {
+    .links-title {
         font-family: customFontCoda;
         font-weight: bold;
         background: -webkit-linear-gradient(#823ba1, #5d38a8);
@@ -125,7 +113,7 @@ function clipboard() {
 }
 
 @media only screen and (max-width: 1200px) {
-    .contact-title {
+    .links-title {
         font-family: customFontCoda;
         font-weight: bold;
         background: -webkit-linear-gradient(#823ba1, #5d38a8);
@@ -152,55 +140,20 @@ function clipboard() {
     text-align: left;
     color: white;
 }
-
 @media only screen and (min-width: 1200px) {
-    .project-img {
-        max-width: 40%;
-        border-radius: 50px;
-        padding: 3px;
-        margin: 3px;
+    .features-img {
+        max-width: 30%;
     }
 }
 
 @media only screen and (max-width: 1200px) {
-    .project-img {
-        max-width: 40%;
-        border-radius: 20px;
-        padding: 3px;
-        margin: 3px;
-    }
-}
-
-.pr {
-    text-align:center;
-}
-
-@media only screen and (max-width: 1200px) {
-    .note {
-        text-align:center;
-        font-size: 3vw;
-        margin-left: 40px;
-        margin-right: 40px;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;;
-        font-style: italic;
-        color: #3a2b4f;
-        padding-top: 20px;
-    }
-}
-
-@media only screen and (min-width: 1200px) {
-    .note {
-        text-align:center;
-        font-size: 26px;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;;
-        font-style: italic;
-        color: #3a2b4f;
-        padding-top: 20px;
+    .features-img {
+        max-width: 80%;
     }
 }
 
 @media only screen and (max-width: 1200px) {
-    .bio {
+    .features {
         text-align: center;
         font-size: 3vw;
         margin-left: 40px;
@@ -212,7 +165,7 @@ function clipboard() {
 }
 
 @media only screen and (min-width: 1200px) {
-    .bio {
+    .features {
         text-align: center;
         font-size: 26px;
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
@@ -223,7 +176,75 @@ function clipboard() {
 
 
 @media only screen and (min-width: 1200px) {
-    .bio-title {
+    .features-title {
+        background: -webkit-linear-gradient(#823ba1, #5d38a8);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        font-family: customFontCoda;
+        font-weight: bold;
+        font-size: 40px;
+        color: white;
+    }
+}
+@media only screen and (max-width: 1200px) {
+    .note {
+        text-align:center;
+        font-size: 3vw;
+        margin-left: 40px;
+        margin-right: 40px;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;;
+        font-style: italic;
+        color: #3a2b4f;
+    }
+}
+
+@media only screen and (min-width: 1200px) {
+    .note {
+        text-align:center;
+        font-size: 26px;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;;
+        font-style: italic;
+        color: #3a2b4f;
+    }
+}
+
+@media only screen and (max-width: 1200px) {
+    .features-title {
+        background: -webkit-linear-gradient(#823ba1, #5d38a8);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        font-family: customFontCoda;
+        font-weight: bold;
+        font-size: 5vw;
+        color: white;
+    }
+}
+
+@media only screen and (max-width: 1200px) {
+    .about {
+        text-align: center;
+        font-size: 3vw;
+        margin-left: 40px;
+        margin-right: 40px;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        color: #d4b5ff;
+        padding-bottom: 40px;
+    }
+}
+
+@media only screen and (min-width: 1200px) {
+    .about {
+        text-align: center;
+        font-size: 26px;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        color: #d4b5ff;
+        padding-bottom: 40px;
+    }
+}
+
+
+@media only screen and (min-width: 1200px) {
+    .about-title {
         background: -webkit-linear-gradient(#823ba1, #5d38a8);
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
@@ -235,7 +256,7 @@ function clipboard() {
 }
 
 @media only screen and (max-width: 1200px) {
-    .bio-title {
+    .about-title {
         background: -webkit-linear-gradient(#823ba1, #5d38a8);
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
@@ -264,11 +285,9 @@ function clipboard() {
 }
 
 .logo {
-    box-shadow: 0px 0px 10px 0px #7231cc;
     display: block;
     margin-left: auto;
     margin-right: auto;
     width: 15vmax;
-    border-radius: 50%;
 }
 </style>
---
+++ src/quake.js
@@ -0,0 +1,7 @@
+import App from './Quake.svelte';
+
+const app = new App({
+	target: document.body,
+});
+
+export default app;
