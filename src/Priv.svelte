be1fdff65bc3426dfe1e9ebb63272bb90bdc8498
Blackoutburst <blackoutburst@hotmail.com>
2021-10-11 13:41:57 +0200

    .

---
+++ src/Priv.svelte
@@ -0,0 +1,29 @@
+<img src="priv/0.png" alt="game" class="features-img">
+<img src="priv/1.png" alt="game" class="features-img">
+<img src="priv/2.png" alt="game" class="features-img">
+<img src="priv/3.png" alt="game" class="features-img">
+<img src="priv/4.png" alt="game" class="features-img">
+<img src="priv/5.png" alt="game" class="features-img">
+<img src="priv/6.png" alt="game" class="features-img">
+<img src="priv/7.png" alt="game" class="features-img">
+<img src="priv/8.png" alt="game" class="features-img">
+<img src="priv/9.png" alt="game" class="features-img">
+<img src="priv/10.png" alt="game" class="features-img">
+<img src="priv/11.png" alt="game" class="features-img">
+<img src="priv/12.png" alt="game" class="features-img">
+<img src="priv/13.png" alt="game" class="features-img">
+<img src="priv/14.png" alt="game" class="features-img">
+<img src="priv/15.png" alt="game" class="features-img">
+<img src="priv/16.png" alt="game" class="features-img">
+<img src="priv/17.png" alt="game" class="features-img">
+<img src="priv/18.png" alt="game" class="features-img">
+<img src="priv/19.png" alt="game" class="features-img">
+<img src="priv/20.png" alt="game" class="features-img">
+<img src="priv/21.png" alt="game" class="features-img">
+<img src="priv/22.png" alt="game" class="features-img">
+
+<style>
+.features-img {
+    text-align: center;
+}
+</style>
---
+++ src/priv.js
@@ -0,0 +1,7 @@
+import App from './Priv.svelte';
+
+const app = new App({
+	target: document.body,
+});
+
+export default app;
