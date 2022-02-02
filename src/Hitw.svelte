3bfb8362d1f485ad8d069368c5084a07c4d82bec
Blackoutburst <blackoutburst@hotmail.com>
2022-02-02 05:01:47 +0100

    Finished the Hitw page

--- src/Hitw.svelte
+++ src/Hitw.svelte
@@ -1,315 +1,80 @@
-<div class="navbar">
-    <a href="index.html"><img src="img/pfp.jpg" alt="logo" class="nav-icon" style="border-radius: 50%"></a>
-    <a href="aow.html"><img src="img/aow.png" alt="aow" class="nav-icon"></a>
-    <a href="hitw.html"><img src="img/hitw.png" alt="hitw" class="nav-icon"></a>
-    <a href="quake.html"><img src="img/quake.png" alt="quake" class="nav-icon"></a>
-    <a href="wally.html"><img src="img/wally.png" alt="wally" class="nav-icon"></a>
-    <a href="pong.html"><img src="img/pong.png" alt="pong" class="nav-icon"></a>
-    <a href="pixel.html"><img src="img/pixel.png" alt="ps" class="nav-icon" style="border-radius: 20px"></a>
-    <a href="falling.html"><img src="img/falling.png" alt="falling" class="nav-icon"></a>
-    <a href="bme.html"><img src="img/bme.png" alt="bme" class="nav-icon"></a>
-</div>
-
 <header>
     <div class="header">
         <img src="img/hitw.png" alt="logo" class="logo">
-        <p class="title">Hole in the Wall</p>
     </div>
 </header>
 
-<div class="about">
-    <h1 class="about-title">About this project</h1>
-    <p>
-        This project started around November 2020<br>
-        At first this was just a joke about a dream that a player (<a href="https://hypixel.net/members/hammyinthewall.154763/" target="_blank" rel="noopener noreferrer">HammyInTheWall</a>) had<br>
-        This dream was a joke version of the game, so I started making this for fun<br>
-        <br>
-        Later I realized I was able to work on something real instead of a joke dream<br>
-        And this is when I started working on a full plugin acting as a server core<br>
-        <br>
-        The plugin today is different from the Hypixel Hole in the Wall<br>
-        You do not compete against players but instead have access to a bunch of options<br>
-        The whole point of this was to create a place to train and experiment<br>
-        <br>
-        Today this plugin is run on our community server and has seen a bunch of improvements<br>
-        New functionality got added on people demands such as duels and custom walls<br>
-    </p>
+<div class="title">Hole in the Wall</div>
+
+<div class="bio">
+    Hole in the Wall is a Spigot plugin, based on the Hypixel game Hole in the wall.<br>
+    It was made for training and community tournament purpose.<br>
+<br>
+This project features: a configuration menu, normal and custom wall, custom game mode<br>
+duel system, score saving accessible via an API and much more.
+</div>
+
+<div class="title">Video</div>
+
+<div class="features">
+    <iframe class="features-vid" src="https://www.youtube.com/embed/yPd0cWm1Kbw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 
 <div class="features">
-
-    <h1 class="features-title">Demo</h1>
-    <iframe class="features-vid" src="https://www.youtube.com/embed/yPd0cWm1Kbw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-    <h1 class="features-title">Features</h1>
-    <p>
-        I really don't have much to explain here about this plugins features<br>
-        Everything is already explained on the <a href="https://github.com/Blackoutburst/HitW-plugin" target="_blank" rel="noopener noreferrer">GitHub</a> of this project<br>
-        <br>
-        <iframe class="features-vid" src="https://www.youtube.com/embed/J37SzCiMifc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <br><br>
-        The Hole in the Wall game is the main features all you do is play freely<br>
-    </p>
-
-    <img src="img/hitw-game.png" alt="game" class="features-img">
-    <p class="note">(Game screenshot on the current map)</p>
-    <p>
-        The option menu to play the game the way you want to play it<br>
-        Every option is explained on the GitHub<br>
-    </p>
-    <img src="img/hitw-menu.png" alt="menu" class="features-img">
-    <p class="note">(The screenshot is a bit outdated now we can see our stats on the bottom right)</p>
-
-    <p>
-        A game summary system so you can see what you did wrong<br>
-    </p>
-    <img src="img/game-summary.png" alt="summary" class="features-img">
-    <p class="note">(All you need to know about your game in one place)</p>
-
-    <p>
-        This plugin also contains a duel system after months on request by the community<br>
-        There is also a <a href="https://www.youtube.com/watch?v=3O-An6CLEao" target="_blank" rel="noopener noreferrer">video (Flashing light)</a> of me coding the duel system<br>
-    </p>
-    <img src="img/duel.png" alt="duel" class="features-img">
-    <p class="note">(You have no idea of much time they asked me for that)</p>
-
-    <p>
-        And for the last big gameplay features we have the party/coop system<br>
-        When inside a party player can play together on the same wall or even make party duels<br>
-    </p>
-    <img src="img/party.png" alt="party" class="features-img">
-    <p class="note">(What ? It's nearly the same as the Hypixel one ? Nah just your imagination)</p>
-    <p>
-        And last, there is an API system used to receive players score<br>
-        This API is used in another project of mine: <a href="wally.html" target="_blank" rel="noopener noreferrer">Wally</a><br>
-    </p>
-    <p class="note">(Not much to says here sadly)</p>
+    <iframe class="features-vid" src="https://www.youtube.com/embed/J37SzCiMifc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 
-<div class="links">
-    <h1 class="links-title">Find this project</h1>
-    <iframe style="width:728px;height:90px;max-width:100%;border:none;display:block;margin:auto" src="https://fr.namemc.com/server/hitw.minesr.com/embed" width="728" height="90"></iframe>
-    <a href="https://github.com/Blackoutburst/HitW-plugin" target="_blank" rel="noopener noreferrer">
-        <img src="img/github.png" alt="github" class="links-img">
-    </a>
+<div class="title">Screenshot</div>
+
+<div class="features">
+    <img src="img/hitw1.jpg" alt="project screenshot" style="width:100%">
+</div>
+<div class="features">
+    <img src="img/hitw2.jpg" alt="project screenshot" style="width:100%">
+</div>
+<div class="features">
+    <img src="img/hitw3.jpg" alt="project screenshot" style="width:100%">
+</div>
+<div class="features">
+    <img src="img/hitw4.jpg" alt="project screenshot" style="width:100%">
 </div>
 
-<div class="links">
-    <h1 class="links-title">Made with</h1>
+<div class="title">Project link</div>
 
-    <a href="https://www.java.com/" target="_blank" rel="noopener noreferrer">
-        <img src="img/java.png" alt="java" class="links-img">
-    </a>
-
-    <a href="https://www.spigotmc.org/" target="_blank" rel="noopener noreferrer">
-        <img src="img/spigot.png" alt="spigot" class="links-img">
-    </a>
+<div class="contact">
+    <div class="contact-link">
+        <a href="https://github.com/Blackoutburst/HitW-plugin" target="_blank" rel="noopener noreferrer">
+            <img src="img/github.png" alt="github" class="links-img" style="width:100%">
+        </a>
+    </div>
 </div>
+<iframe style="width:728px;height:90px;max-width:100%;border:none;display:block;margin:auto" src="https://fr.namemc.com/server/hitw.minesr.com/embed" width="728" height="90"></iframe>
 
-<div id="clipboard"></div>
+<div class="title">Made with</div>
+
+<div class="contact">
+    <div class="contact-link">
+        <a href="https://www.java.com/" target="_blank" rel="noopener noreferrer">
+            <img src="img/java.png" alt="java" style="width:100%">
+        </a>
+    </div>
+    <div class="contact-link">
+        <a href="https://www.spigotmc.org/" target="_blank" rel="noopener noreferrer">
+            <img src="img/spigot.png" alt="spigot" style="width:100%">
+        </a>
+    </div>
+
+</div>
 
 <div class="footer">
     <p class="footer-title">© Blackoutburst</p>
 </div>
 
 <style>
-@font-face {
-  font-family: customFontCoda;
-  src: url("../font/coda.ttf");
-}
-
-@media only screen and (min-width: 1200px) {
-    .features-vid {
-        width: 60vw;
-        height: calc(60vw * 9 / 16);
+    .logo {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 30vmin;
     }
-}
-
-@media only screen and (max-width: 1200px) {
-    .features-vid {
-        width: 50vw;
-        height: calc(50vw * 9 / 16);
-    }
-}
-
-@media only screen and (max-width: 1200px) {
-    .links-img {
-        max-width: 15vw;
-        padding: 10px;
-        margin: 10px;
-    }
-}
-
-@media only screen and (min-width: 1200px) {
-    .links-img {
-        max-width: 7%;
-        padding: 10px;
-        margin: 10px;
-    }
-}
-
-.links {
-    margin-top: 80px;
-    padding: 10px;
-    text-align: center;
-    color: white;
-}
-
-@media only screen and (min-width: 1200px) {
-    .links-title {
-        font-family: customFontCoda;
-        font-weight: bold;
-        background: #bf00ff;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        font-size: 40px;
-        color: white;
-    }
-}
-
-@media only screen and (max-width: 1200px) {
-    .links-title {
-        font-family: customFontCoda;
-        font-weight: bold;
-        background: #bf00ff;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        font-size: 5vw;
-        color: white;
-    }
-}
-
-@media only screen and (min-width: 1200px) {
-    .features-img {
-        max-width: 50%;
-    }
-}
-
-@media only screen and (max-width: 1200px) {
-    .features-img {
-        max-width: 80%;
-    }
-}
-
-@media only screen and (max-width: 1200px) {
-    .features {
-        text-align: center;
-        font-size: 3vw;
-        margin-left: 40px;
-        margin-right: 40px;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        color: #d4b5ff;
-        padding-bottom: 40px;
-    }
-}
-
-@media only screen and (min-width: 1200px) {
-    .features {
-        text-align: center;
-        font-size: 26px;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        color: #d4b5ff;
-        padding-bottom: 40px;
-    }
-}
-
-
-@media only screen and (min-width: 1200px) {
-    .features-title {
-        background: #bf00ff;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        font-family: customFontCoda;
-        font-weight: bold;
-        font-size: 40px;
-        color: white;
-    }
-}
-@media only screen and (max-width: 1200px) {
-    .note {
-        text-align:center;
-        font-size: 3vw;
-        margin-left: 40px;
-        margin-right: 40px;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;;
-        font-style: italic;
-        color: #4f396e;
-    }
-}
-
-@media only screen and (min-width: 1200px) {
-    .note {
-        text-align:center;
-        font-size: 26px;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;;
-        font-style: italic;
-        color: #4f396e;
-    }
-}
-
-@media only screen and (max-width: 1200px) {
-    .features-title {
-        background: #bf00ff;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        font-family: customFontCoda;
-        font-weight: bold;
-        font-size: 5vw;
-        color: white;
-    }
-}
-
-@media only screen and (max-width: 1200px) {
-    .about {
-        text-align: center;
-        font-size: 3vw;
-        margin-left: 40px;
-        margin-right: 40px;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        color: #d4b5ff;
-        padding-bottom: 40px;
-    }
-}
-
-@media only screen and (min-width: 1200px) {
-    .about {
-        text-align: center;
-        font-size: 26px;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        color: #d4b5ff;
-        padding-bottom: 40px;
-    }
-}
-
-
-@media only screen and (min-width: 1200px) {
-    .about-title {
-        background: #bf00ff;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        font-family: customFontCoda;
-        font-weight: bold;
-        font-size: 40px;
-        color: white;
-    }
-}
-
-@media only screen and (max-width: 1200px) {
-    .about-title {
-        background: #bf00ff;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        font-family: customFontCoda;
-        font-weight: bold;
-        font-size: 5vw;
-        color: white;
-    }
-}
-
-.logo {
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 15vmax;
-}
 </style>
