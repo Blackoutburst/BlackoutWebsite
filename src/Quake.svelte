282caebc58bc7528791236e32fd6437239a66072
Blackoutburst <blackoutburst@hotmail.com>
2022-03-05 21:17:22 +0100

    Fixed quake video link

--- src/Quake.svelte
+++ src/Quake.svelte
@@ -17,7 +17,7 @@
 <div class="title">Video</div>
 
 <div class="features">
-    <iframe class="features-vid" src="https://www.youtube.com/embed/oK-eeDDhNRs?list=LL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe class="features-vid" src="https://www.youtube.com/embed/oK-eeDDhNRs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 
 <div class="features">
