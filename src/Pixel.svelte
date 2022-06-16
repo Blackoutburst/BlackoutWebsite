38a5e5d26b7219f24e0bb6da8ce38c28986f3512
Blackoutburst <blackoutburst@hotmail.com>
2022-06-16 21:58:05 +0200

    Added pixel starship

--- src/Pixel.svelte
+++ src/Pixel.svelte
@@ -1,70 +1,46 @@
-<header>
-    <div class="header">
-        <img src="img/pixel.png" alt="logo" class="logo">
-    </div>
-</header>
+<script>
+	import Header from "./components/Header.svelte"
+    import Title from "./components/Title.svelte"
+    import Bio from "./components/Bio.svelte"
+    import Image from "./components/Image.svelte"
+    import SubTitle from "./components/SubTitle.svelte"
+    import LinkIcon from "./components/LinkIcon.svelte"
+    import Footer from "./components/Footer.svelte"
+</script>
 
-<div class="title">Pixel Starship</div>
+<Header/>
 
-<div class="bio">
-    Pixel Starship is a Android game,<br>
-    in this game your goal is to go as far as possible.<br>
-    This is also my first game ever published on the Play Store!<br>
+<Title label='Pixel Starship'/>
+
+<Bio text="Pixel Starship is a Android game,
+in this game your goal is to go as far as possible.
+This is also my first game ever published on the Play Store!
+"/>
+
+<SubTitle label='Screenshot'/>
+
+<Image imagePath='img/pixel1.jpg'/>
+<Image imagePath='img/pixel2.jpg'/>
+<Image imagePath='img/pixel3.jpg'/>
+<Image imagePath='img/pixel4.jpg'/>
+
+<SubTitle label='Project link'/>
+
+<div class="flex flex-wrap gap-5 flex-rows justify-center items-center w-3/4 m-auto">
+    <LinkIcon link="https://play.google.com/store/apps/details?id=com.blackoutburst.pixelstarship" imagePath="img/play-store.png" />
 </div>
 
-<div class="title">Screenshot</div>
+<SubTitle label='Made with'/>
 
-<div class="features">
-    <img src="img/pixel1.jpg" alt="project screenshot" style="width:100%" class="features-image">
+<div class="flex flex-wrap gap-5 flex-rows justify-center items-center w-3/4 m-auto">
+    <LinkIcon link="https://www.java.com/" imagePath="img/java.png" />
+    <LinkIcon link="https://www.android.com/" imagePath="img/android.png" />
 </div>
 
-<div class="features">
-    <img src="img/pixel2.jpg" alt="project screenshot" style="width:100%" class="features-image">
-</div>
+<Footer/>
 
-<div class="features">
-    <img src="img/pixel3.jpg" alt="project screenshot" style="width:100%" class="features-image">
-</div>
-
-<div class="features">
-    <img src="img/pixel4.jpg" alt="project screenshot" style="width:100%" class="features-image">
-</div>
-
-<div class="title">Project link</div>
-
-<div class="contact">
-    <div class="contact-link">
-        <a href="https://play.google.com/store/apps/details?id=com.blackoutburst.pixelstarship" target="_blank" rel="noopener noreferrer">
-            <img src="img/play-store.png" alt="github" class="links-img" style="width:100%">
-        </a>
-    </div>
-</div>
-
-<div class="title">Made with</div>
-
-<div class="contact">
-    <div class="contact-link">
-        <a href="https://www.java.com/" target="_blank" rel="noopener noreferrer">
-            <img src="img/java.png" alt="java" style="width:100%">
-        </a>
-    </div>
-    <div class="contact-link">
-        <a href="https://www.android.com/" target="_blank" rel="noopener noreferrer">
-            <img src="img/android.png" alt="spigot" style="width:100%">
-        </a>
-    </div>
-</div>
-
-<div class="footer">
-    <p class="footer-title">© Blackoutburst</p>
-</div>
-
-<style>
-    .logo {
-        border-radius: 1em;
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        width: 30vmin;
-    }
+<style lang="postcss" global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
 </style>
