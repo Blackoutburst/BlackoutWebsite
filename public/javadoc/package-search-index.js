49e429eb6d3c6cf0aef4d6b3e73954ded687d9e4
Florian Chanson <f.chanson@barbablanka.com>
2022-06-30 11:15:24 +0200

    Temporary hosting a project javadoc

---
+++ public/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.barbablanka.henry.mmsreflect"}];updateSearchResults();
---
+++ public/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.barbablanka.henry.mmsreflect","l":"Reflector"}];updateSearchResults();
