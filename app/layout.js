6524e92d421a6085ea4ccb1458c40e8934082d29
Blackoutburst <blackoutburst@hotmail.com>
2025-05-22 22:01:59 +0200

    fixed build

--- app/layout.js
+++ app/layout.js
@@ -8,10 +8,10 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <head>
                 <title>Blackoutburst</title>
-                    <meta charset="UTF-8">
-                    <meta name="description" content="Blackoutburst website">
-                    <meta name="keywords" content="Blackoutburst, developer, Kotlin, osu, GitHub, Discord">
-                    <meta name="author" content="Blackoutburst">
+                    <meta charset="UTF-8"/>
+                    <meta name="description" content="Blackoutburst website"/>
+                    <meta name="keywords" content="Blackoutburst, developer, Kotlin, osu, GitHub, Discord"/>
+                    <meta name="author" content="Blackoutburst"/>
             </head>
             <body className={`${inter.className} scrollbar`}>{children}</body>
         </html>
