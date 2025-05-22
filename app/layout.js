35e43979e565249f119bf29948c29d13240b1447
Blackoutburst <blackoutburst@hotmail.com>
2025-05-22 21:48:55 +0200

    total rework minimalist

--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,6 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-    title: 'Florian "Blackoutburst" Chanson',
-    description: "Welcome to my website! I'm A French developer specialized in JVM technologies, Currently working at Barbablanka on Web/Android projects."
-}
-
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
--- app/page.js
+++ app/page.js
@@ -1,21 +1,49 @@
-import HomeSection from "@/components/HomeSection"
-import AboutMeSection from "@/components/AboutMeSection"
-import TechnoSections from "@/components/TechnoSections"
-import ProjectsSections from "@/components/ProjectsSections"
-import SocialsSections from "@/components/SocialsSection"
+import Image from "next/image"
+
 
 export default function Home() {
     return (
-        <main className="xl:m-20">
-            <div className={"relative xl:border-2 xl:border-dashed xl:border-[#28193e] xl:rounded-2xl z-50 overflow-hidden xl:shadow-sm bg-neutral-900"}>
-                <HomeSection/>
-                <div className={"relative select-none w-full auto -z-40 rotate-180 -translate-y-[1px] scale-x-[200%] md:scale-x-100"}>
-                    <img alt={"wave"} className={"-z-40"} src={"/wave.svg"}/>
+        <main className="flex w-full justify-center items-center w-screen h-screen">
+            <Image
+                width={0} 
+                height={0}
+                alt={"background"}
+                src="/bg.svg"
+                alt="Background"
+                className="absolute w-full h-full object-cover opacity-70"
+            />
+            <div className="flex flex-col justify-center items-center gap-6 m-4">
+                <h1>
+                    BLACKOUTBURST
+                </h1>
+                <h2>
+                    About
+                </h2>
+                <p className="text-center">
+                    I'm a developer with strong ability in the JVM ecosystem<br/>
+                    When I'm not working I usually play video games<br/>
+                    Currently working at <a href="https://primal.tech/">Primal</a> as Android dev
+                </p>
+                <h2>
+                    Contact
+                </h2>
+                <p>
+                    If for some reaons you need/want to contact me:<br/>
+                    - <a target="_blank" rel="noopener noreferrer" href="https://discord.com/">Discord</a>: blackoutburst (fast reply)<br/>
+                    - <a target="_blank" rel="noopener noreferrer" href="mailto:me@blackoutburst.com">E-Mail</a>: me@blackoutburst.com (slow reply)
+                </p>
+                <h2>
+                    Links
+                </h2>
+                <div className="flex justify-center items-center gap-4">
+                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/Blackoutburst/" className={"z-50 hover:scale-110 duration-300"}>
+                        <Image width={0} height={0} alt={"github logo"} className={"icons select-none cursor-pointer z-50"} src={"/github-logo.svg"}/>
+                    </a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/@blackoutburst803/" className={"z-50 hover:scale-110 duration-300"}>
+                        <Image width={0} height={0} alt={"youtube logo"} className={"icons select-none cursor-pointer z-50"} src={"/youtube-logo.svg"}/>
+                    </a>
                 </div>
-                <AboutMeSection/>
-                <TechnoSections/>
-                <ProjectsSections/>
-                <SocialsSections/>
+                <Image width={256} height={256} alt={"pic"} className={"pic select-none cursor-pointer z-50"} src={"/pp.webp"}/>
             </div>
         </main>
     )
