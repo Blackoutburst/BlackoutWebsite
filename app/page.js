143786664c355484f90b2074fb8f1f38ed1e74ed
Blackoutburst <blackoutburst@hotmail.com>
2025-05-22 22:38:05 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,40 +11,38 @@ export default function Home() {
                 alt="background"
                 className="absolute -z-50 w-screen h-screen object-cover opacity-70 overflow-hidden"
             />
-            <main className="flex w-full justify-center items-center w-full overflow-hidden">
-                <div className="flex flex-col justify-center items-center gap-6 m-4 mt-8">
-                    <h1>
-                        BLACKOUTBURST
-                    </h1>
-                    <h2>
-                        About
-                    </h2>
-                    <p className="text-center">
-                        I&apos;m a developer with strong abilities in the JVM ecosystem<br/>
-                        When I&apos;m not working I usually play video games<br/>
-                        Currently working at <a href="https://primal.tech/">Primal</a> as Android dev
-                    </p>
-                    <h2>
-                        Contact
-                    </h2>
-                    <p>
-                        If for some reasons you need/want to contact me:<br/>
-                        - <a target="_blank" rel="noopener noreferrer" href="https://discord.com/">Discord</a>: blackoutburst (fast reply)<br/>
-                        - <a target="_blank" rel="noopener noreferrer" href="mailto:me@blackoutburst.com">E-Mail</a>: me@blackoutburst.com (slow reply)
-                    </p>
-                    <h2>
-                        Links
-                    </h2>
-                    <div className="flex justify-center items-center gap-4">
-                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/Blackoutburst/" className={"z-50 hover:scale-110 duration-300"}>
-                            <Image width={0} height={0} alt={"github logo"} className={"icons select-none cursor-pointer z-50"} src={"/github-logo.svg"}/>
-                        </a>
-                        <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/@blackoutburst803/" className={"z-50 hover:scale-110 duration-300"}>
-                            <Image width={0} height={0} alt={"youtube logo"} className={"icons select-none cursor-pointer z-50"} src={"/youtube-logo.svg"}/>
-                        </a>
-                    </div>
-                    <Image width={256} height={256} alt={"pic"} className={"pic select-none cursor-pointer z-50"} src={"/pp.webp"}/>
+            <main className="flex flex-col w-full justify-center items-center gap-6 m-4 mt-8 overflow-hidden">
+                <h1>
+                    BLACKOUTBURST
+                </h1>
+                <h2>
+                    About
+                </h2>
+                <p className="text-center">
+                    I&apos;m a developer with strong abilities in the JVM ecosystem<br/>
+                    When I&apos;m not working I usually play video games<br/>
+                    Currently working at <a href="https://primal.tech/">Primal</a> as Android dev
+                </p>
+                <h2>
+                    Contact
+                </h2>
+                <p>
+                    If for some reasons you need/want to contact me:<br/>
+                    - <a target="_blank" rel="noopener noreferrer" href="https://discord.com/">Discord</a>: blackoutburst (fast reply)<br/>
+                    - <a target="_blank" rel="noopener noreferrer" href="mailto:me@blackoutburst.com">E-Mail</a>: me@blackoutburst.com (slow reply)
+                </p>
+                <h2>
+                    Links
+                </h2>
+                <div className="flex justify-center items-center gap-4">
+                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/Blackoutburst/" className={"z-50 hover:scale-110 duration-300"}>
+                        <Image width={0} height={0} alt={"github logo"} className={"icons select-none cursor-pointer z-50"} src={"/github-logo.svg"}/>
+                    </a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/@blackoutburst803/" className={"z-50 hover:scale-110 duration-300"}>
+                        <Image width={0} height={0} alt={"youtube logo"} className={"icons select-none cursor-pointer z-50"} src={"/youtube-logo.svg"}/>
+                    </a>
                 </div>
+                <Image width={256} height={256} alt={"pic"} className={"pic select-none cursor-pointer z-50"} src={"/pp.webp"}/>
             </main>
         </>
     )
